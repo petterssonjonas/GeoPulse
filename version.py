20785299b29d870aa854d185cc8d154811099ebb
Jonas Pettersson <jpettersson@gmail.com>
2026-03-06 13:17:27 +0100

    Version 0.90.4, with workflows

--- version.py
+++ version.py
@@ -4,4 +4,4 @@ After changing __version__, run:  ./scripts/sync-version.py
 to update packaging/geopulse.spec, packaging/AppImageBuilder.yml, and README.md.
 """
 
-__version__ = "0.90.3"
+__version__ = "0.90.4"
